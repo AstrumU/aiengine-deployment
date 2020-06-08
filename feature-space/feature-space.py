1bcf6bbce569385f20b109bf1ef7085ac27f3a02
Frank Zhang <57238981+frank-zhang-astrumu@users.noreply.github.com>
2020-06-08 11:37:46 -0700

    Update feature-space.py

--- feature-space/feature-space.py
+++ feature-space/feature-space.py
@@ -7,12 +7,19 @@
 # import the necessary packages
 from pyspark.sql import SparkSession
 from flask import abort, jsonify, Flask, request, Response
-import os
+
 
 
 # initialize our Flask application and the AI model
 app = Flask(__name__)
 model = None
+
+#set up azure blob storage container path
+blobpath = "wasbs://databricks-gold@dsdatalakev3.blob.core.windows.net" 
+student_profile_path = blobpath + "/AI_Engine/student_360_profile_engine_parquet/"
+campaign_entity_json_path = blobpath + "/AI_Engine/student_360_profile_engine_parquet/"
+campaign_feature_space_parquet_empty_path  =  blobpath + "/AI_Engine/campaign_feature_space_parquet_empty/'
+campaign_feature_space_parquet_update_path =  blobpath + "dbfs:/mnt/databricks-gold/AI_Engine/campaign_feature_space_parquet_update/“
 
 # Grap Spark Session which is the entry point for the cluster resources
 def CreateSparkSession():
@@ -30,39 +37,22 @@ def CreateSparkSession():
 	    "fs.azure.account.key.dsdatalakev3.blob.core.windows.net", \
 	    "2xnju/y0WIPj1JUQ7KA1HvJTaLJo3HyFMMZ/62MTa91ju6G4GYCk2Ml5bfK/IiR9ZUFHiGNQZ+V16ByV5DrP7Q=="
     )
-    #set up azure blob storage container path
-    Setpath()   
-    return spark
-
-def Setpath():
-    global blobpath
-    blobpath = "wasbs://databricks-gold@dsdatalakev3.blob.core.windows.net" 
-
-@app.route('/feature-space', methods=['POST'])
+  
+     return spark
 
 
+@app.route('/featurespace', methods=['POST'])
+def FeatureSpace():
+    student_profile_id = spark.read.option("mergeSchema","true").parquet(student_profile_engine_parquet).select('AstrumU_UUID').distinct()
+    campaign_entity_id = spark.read.format("json").load(campaign_entity_json_path1).select(col('id').alias('Campaign_ID')).distinct()
+    campaign_feture_space_empty =  spark.read.option("mergeSchema","true").parquet(campaign_feature_space_parquet_empty_path)
+    
+    student_campaign_id = student_profile_id.crossJoin(campaign_entity_id).distinct()
+    campaign_feature_space_update = campaign_feture_space_empty.join(student_campaign_id, "AstrumU_UUID", 'outer')
+    campaign_feature_space_update.write.option("mergeSchema", "true").mode("overwrite").parquet(campaign_feature_space_parquet_update_path)   
+       
 
 if __name__ == "__main__":
     spark = CreateSparkSession()
-    readcsvpath = blobpath + "/Common_Data/airports.csv"
     
-    student_profile = spark.read.option("mergeSchema", "true").parquet("data/test_table")
-    student_profile_engine.write.option("mergeSchema", "true").mode("overwrite").parquet(student_profile_engine_parquet)
-
-    app.run(host='0.0.0.0', port=8080)
-
-
-    df = spark.read.option("header", "true")\
-          .option("delimiter", ",")\
-          .option("inferSchema", "true")\
-          .csv(readcsvpath)
-
-    df.registerTempTable("airlines")
-    result = spark.sql("""
-          select state
-          from airlines    
-     """)
-
-    #result.write.mode("overwrite").parquet("wasbs://databricks-gold@dsdatalakev3.blob.core.windows.net/Common_Data/airport-result.parquet")
-    #sdf.show(sdf)
-    print(df)
+    app.run(host='0.0.0.0', port=5000)
